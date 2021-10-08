50ff954d0ee5424089984a0f0e039d0f24338f15
Sanity.io <hello@sanity.io>
2021-10-08 04:52:26 -0400

    Initial commit

---
+++ studio/src/dashboardConfig.js
@@ -0,0 +1,57 @@
+export default {
+  widgets: [
+    { name: "structure-menu" },
+    {
+      name: "project-info",
+      options: {
+        __experimental_before: [
+          {
+            name: "netlify",
+            options: {
+              description:
+                "NOTE: Because these sites are static builds, they need to be re-deployed to see the changes when documents are published.",
+              sites: [
+                {
+                  buildHookId:
+                    "616006c727f46d009205bab4",
+                  title: "Sanity Studio",
+                  name: "sanity-gatsby-blog-3-studio-ch48ysi1",
+                  apiId: "70b2c407-2de5-40ad-955f-05d23532b06d",
+                },
+                {
+                  buildHookId: "616006c75d40dc009240148a",
+                  title: "Blog Website",
+                  name: "sanity-gatsby-blog-3-web-dnkdb4p7",
+                  apiId: "a15415bf-f9f6-46f1-a917-ffad27002068",
+                },
+              ],
+            },
+          },
+        ],
+        data: [
+          {
+            title: "GitHub repo",
+            value:
+              "https://github.com/bgoonz/sanity-gatsby-blog3",
+            category: "Code",
+          },
+          {
+            title: "Frontend",
+            value: "https://sanity-gatsby-blog-3-web-dnkdb4p7.netlify.app",
+            category: "apps",
+          },
+        ],
+      },
+    },
+    { name: "project-users", layout: { height: "auto" } },
+    {
+      name: "document-list",
+      options: {
+        title: "Recent blog posts",
+        order: "_createdAt desc",
+        types: ["post"],
+      },
+      layout: { width: "medium" },
+    },
+  ],
+};
---
+++ web/client-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  sanity: {
+    projectId:
+      process.env.GATSBY_SANITY_PROJECT_ID || "25btqj05",
+    dataset: process.env.GATSBY_SANITY_DATASET || "production",
+  },
+};
